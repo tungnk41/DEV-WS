f6cae8ba135902d38b1cf231a95813d3a092bc07
tung6.nguyen <tung6.nguyen@lge.com>
2025-08-11 17:56:02 +0700

    update

--- AoC_2024/Day5/main.py
+++ AoC_2024/Day5/main.py
@@ -3,4 +3,11 @@
 # Read the input file
 filepath = "AoC_2024\\Day5\\input.txt"
 with open(filepath, "r", encoding="utf-8") as file:
-    grid = [line.strip() for line in file.readlines()]
+    data = [line for line in file.read().split("\n") if line.strip()]
+    block_1 = [line for line in data if '|' in line]
+    block_2 = [line for line in data if ',' in line]
+
+    print(data)
+    print(block_1)
+    print(block_2)
+
