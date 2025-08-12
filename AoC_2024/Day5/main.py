572ccccb8daac657818defeb0c2ec6ebf07d9eb2
tung6.nguyen <tung6.nguyen@lge.com>
2025-08-12 19:18:11 +0900

    update

--- AoC_2024/Day5/main.py
+++ AoC_2024/Day5/main.py
@@ -1,8 +1,8 @@
+import pathlib
 
-
+file_path = pathlib.Path(__file__).parent.resolve().joinpath("input.txt")
 # Read the input file
-filepath = "AoC_2024\\Day5\\input.txt"
-with open(filepath, "r", encoding="utf-8") as file:
+with open(file_path, "r", encoding="utf-8") as file:
     data = [line for line in file.read().split("\n") if line.strip()]
     block_1 = [line for line in data if '|' in line]
     block_2 = [line for line in data if ',' in line]
