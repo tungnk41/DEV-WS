8045aab9477cbbadab1ce12c16c21e6697c2e0c5
tungnk41 <tung.nk41@gmail.com>
2025-08-12 22:11:58 +0700

    update

--- AoC_2024/Day5/main.py
+++ AoC_2024/Day5/main.py
@@ -1,63 +1,86 @@
 import pathlib
-
-
-class Node:
-    def __init__(self, idx):
-        self.idx = idx
-        self.next = []
-
-    def __str__(self):
-        return str(self.idx)
-
+from itertools import permutations
+import pprint
+from collections import defaultdict
 
 # Read the input file
 block_1 = []
 block_2 = []
-file_path = pathlib.Path(__file__).parent.resolve().joinpath("input.txt")
-with open(file_path, "r", encoding="utf-8") as file:
-    data = [line for line in file.read().split("\n") if line.strip()]
-    block_1 = [
-        list(map(int, sublist))
-        for sublist in (line.strip().split("|") for line in data if "|" in line)
-    ]
-    block_2 = [
-        list(map(int, sublist))
-        for sublist in (line.strip().split(",") for line in data if "," in line)
-    ]
-    # print(block_1)
-    # print(block_2)
+m = defaultdict(list)
+
+
+def init():
+    global block_1, block_2, m
+    file_path = pathlib.Path(__file__).parent.resolve().joinpath("input.txt")
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = [line for line in file.read().split("\n") if line.strip()]
+        block_1 = [
+            list(map(int, sublist))
+            for sublist in (line.strip().split("|") for line in data if "|" in line)
+        ]
+        block_2 = [
+            list(map(int, sublist))
+            for sublist in (line.strip().split(",") for line in data if "," in line)
+        ]
+        for item in block_1:
+            if item[0] not in m:
+                m[item[0]] = []
+            if item[1] not in m:
+                m[item[1]] = []
+            m[item[0]].append(item[1])
+        # for k, v in m.items():
+        #     print(k)
+        #     print(v)
+        # print(block_1)
+        # print(block_2)
+
+
+def verify(lst):
+    prev = None
+    isBreak = False
+    for element in lst:
+        current = element if element in m else None
+        if current == None:
+            isBreak = True
+            break
+        if prev == None:  # first index
+            prev = current
+        else:
+            if current in m[prev]:
+                prev = current
+            else:
+                isBreak = True
+    return not isBreak
 
 
 def part_1():
-    m = {}
-    for list in block_1:
-        if list[0] not in m:
-            m[list[0]] = Node(list[0])
-        if list[1] not in m:
-            m[list[1]] = Node(list[1])
-        m[list[0]].next.append(m[list[1]])
-
     list_ans = []
-    for list in block_2:
-        node = None
-        isBreak = False
-        for idx, element in enumerate(list):
-            current = m[element] if element in m else None
-            if current == None:
-                isBreak = True
-                break
-            if node == None:  # first index
-                node = current
-            else:
-                if current in node.next:
-                    node = current
-                else:
-                    isBreak = True
-                    break
-        if not isBreak:
-            list_ans.append(list)
+    for lst in block_2:
+        if verify(lst):
+            list_ans.append(lst)
     ans = 0
-    for list in list_ans:
-        idx_med = len(list) // 2
-        ans += list[idx_med]
+    for lst in list_ans:
+        idx_med = len(lst) // 2
+        ans += lst[idx_med]
     print(ans)
+
+
+def part_2():
+    list_ans = []
+    for l in block_2:
+        if not verify(l):
+            for per in permutations(l):
+                if verify(per):
+                    list_ans.append(per)
+                    break
+    print(list_ans)
+    ans = 0
+    for lst in list_ans:
+        idx_med = len(lst) // 2
+        ans += lst[idx_med]
+    print(ans)
+
+
+init()
+# part_1()
+part_2()
